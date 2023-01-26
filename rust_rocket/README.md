e35d1df895788004595dc26e957bf829ef0e8332
Nkrumah <nkrumahthis@gmail.com>
2023-01-26 19:46:03 +0000

    set up and welcome on home route

---
+++ rust_rocket/README.md
@@ -0,0 +1,5 @@
+# Rust Assorted Jollof
+
+## Guides
+
+- <https://betterprogramming.pub/creating-a-web-server-using-rust-rocket-1e4939e582df>
--- rust_rocket/src/main.rs
+++ rust_rocket/src/main.rs
@@ -1,3 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+// import Rocket
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, welcome to Assorted Jollof"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
