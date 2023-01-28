f8bdea9c8119a3de9ef01df9ef7240a3cc8017f9
Nkrumah <nkrumahthis@gmail.com>
2023-01-28 19:28:30 +0000

    rename typescript directory

--- typescript_express/index.ts
+++ typescript_express/index.ts
@@ -1,11 +1,15 @@
-import express from "express"
+import express, {Request, Response} from "express"
+import userRouter from "./user.router"
 
 const app = express();
+app.use(express.json())
 const port = 8000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res:Response) => {
   res.send('Welcome to Assorted Jollof');
 });
+
+app.use("/users", userRouter)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
