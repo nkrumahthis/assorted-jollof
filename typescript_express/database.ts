06e673da5c88b7bc63b283cb874f16ba800d0e18
Nkrumah <nkrumahthis@gmail.com>
2023-01-29 22:15:29 +0000

    use better-sqlite3

--- typescript_express/database.ts
+++ typescript_express/database.ts
@@ -1,4 +1,4 @@
-import { Database } from "sqlite3";
+import Database from "better-sqlite3";
 import { readFileSync } from "fs";
 import {resolve} from "path"
 
@@ -9,7 +9,10 @@ console.log("setting up db")
 // create tables if they don't exist
 const CREATE_TABLES = readFileSync(resolve("..", "main.sql")).toString();
 const SEED_DATA = readFileSync(resolve("..", "seed.sql")).toString();
-const db = new Database("../main.db");;
+
+const db = new Database("../main.db");
+db.pragma('journal_mode = WAL');
+
 db.exec(CREATE_TABLES);
 db.exec(SEED_DATA);
 
