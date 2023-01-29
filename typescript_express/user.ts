a6d3debdfcfd9eaafe98312a371ed73cf7363c05
Nkrumah <nkrumahthis@gmail.com>
2023-01-29 23:04:22 +0000

    user from database

--- typescript_express/user.ts
+++ typescript_express/user.ts
@@ -1,3 +1,5 @@
+import db from "./database";
+
 export type User = {
 	id: number | undefined;
 	name: string;
@@ -5,44 +7,35 @@ export type User = {
 };
 
 export const ERROR_USER_NOT_FOUND = new Error("user not found");
-
-let data: User[] = [
-	{ id: 0, name: "Dana", phone: "080323789010" },
-	{ id: 1, name: "Eric", phone: "02033342312" },
-	{ id: 2, name: "Frank", phone: "02033342312" },
-	{ id: 3, name: "Gerald", phone: "02033342312" },
-	{ id: 4, name: "Harry", phone: "02033342312" },
-];
+export const ERROR_NOT_IMPLEMENTED = new Error("not yet implemented");
 
 export function all(): User[] {
+	let data: User[] = db.prepare("SELECT * FROM users").all();
 	return data;
 }
 
 export function get(id: number): User {
-	let found = data.some((item) => item.id == id);
-	if (!found) throw ERROR_USER_NOT_FOUND;
-
-	return data[id];
+	let data: User = db.prepare(`SELECT * FROM users WHERE id=?`).get(id);
+	if (!data) throw ERROR_USER_NOT_FOUND;
+	return data;
 }
 
 export function create(user: User): User {
-	user.id = data.length - 1;
-	data.push(user);
-	return data[data.length - 1];
+	// The following are equivalent.
+	const stmt = db.prepare(
+		"INSERT INTO users (name, phone) VALUES (:name, :phone)"
+	);
+
+	const info = stmt.run(user);
+	const data = get(Number(info.lastInsertRowid))
+
+	return data;
 }
 
 export function update(id: number, user: User): User {
-	let found = data.some((item) => item.id == id);
-	if (!found) throw ERROR_USER_NOT_FOUND;
-
-	data = data.filter((item) => item.id !== id);
-	return data[id];
+	throw ERROR_NOT_IMPLEMENTED;
 }
 
 export function remove(id: number): boolean {
-	let found = data.some((item) => item.id == id);
-	if (!found) throw ERROR_USER_NOT_FOUND;
-
-	data = data.filter((item) => item.id !== id);
-	return found;
+	throw ERROR_NOT_IMPLEMENTED;
 }
