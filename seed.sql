e1c67c3f002df5e47b9aea4249127dca84c1de78
Nkrumah <nkrumahthis@gmail.com>
2023-01-29 22:12:46 +0000

    fix seed sql

--- seed.sql
+++ seed.sql
@@ -1,18 +1,18 @@
-INSERT OR REPLACE INTO users VALUES
+INSERT OR REPLACE INTO users(id, name, phone) VALUES
     (1, 'Adam Asante', '+1234567890'),
     (2, 'Bill Boakye', '+9872340192'),
     (3, 'Charles C', '+7232019923');
 
-INSERT OR REPLACE INTO orders (id, packs, user_id, location, status, created_at) VALUES
-    (1, 1, 1, "Ashaiman Junction", "FULFILLED", NULL),
-    (2, 2, 2, "Burma Camp", "FULFILLED", NULL),
-    (3, 1, 3, "Next to China Mall", "CANCELLED", NULL),
-    (4, 2, 1, "Ashaiman Junction", "CANCELLED", NULL),
-    (5, 3, 2, "Burma Camp", "FULFILLED", NULL),
-    (6, 1, 1, "Ashaiman Junction", "FULFILLED", NULL),
-    (7, 1, 3, "Next to China Mall", "PENDING", NULL),
-    (8, 2, 1, "Ashaiman Junction", "PENDING", NULL),
-    (9, 2, 2, "Ashaiman Junction", "PENDING", NULL);
+INSERT OR REPLACE INTO orders(id, packs, user_id, location, status, created_at) VALUES
+    (1, 1, 1, 'Ashaiman Junction', 'FULFILLED', NULL),
+    (2, 2, 2, 'Burma Camp', 'FULFILLED', NULL),
+    (3, 1, 3, 'Next to China Mall', 'CANCELLED', NULL),
+    (4, 2, 1, 'Ashaiman Junction', 'CANCELLED', NULL),
+    (5, 3, 2, 'Burma Camp', 'FULFILLED', NULL),
+    (6, 1, 1, 'Ashaiman Junction', 'FULFILLED', NULL),
+    (7, 1, 3, 'Next to China Mall', 'PENDING', NULL),
+    (8, 2, 1, 'Ashaiman Junction', 'PENDING', NULL),
+    (9, 2, 2, 'Ashaiman Junction', 'PENDING', NULL);
 
 INSERT OR REPLACE INTO payments(id, amount, user_id, order_id, created_at) VALUES
     (1, 45.00, 1, 1, NULL),
