85a07670b0baa8fcda12f1eee52dd1e8985c372f
Nkrumah <nkrumahthis@gmail.com>
2023-01-29 21:09:37 +0000

    setting up db

---
+++ typescript_express/database.ts
@@ -0,0 +1,16 @@
+import { Database } from "sqlite3";
+import { readFileSync } from "fs";
+import {resolve} from "path"
+
+// https://sagot.dev/en/articles/sqlite-typescript/
+
+console.log("setting up db")
+
+// create tables if they don't exist
+const CREATE_TABLES = readFileSync(resolve("..", "main.sql")).toString();
+const SEED_DATA = readFileSync(resolve("..", "seed.sql")).toString();
+const db = new Database("../main.db");;
+db.exec(CREATE_TABLES);
+db.exec(SEED_DATA);
+
+export default db;
