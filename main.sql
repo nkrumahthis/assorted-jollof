34958bfdd854b8758c3254d42bc400ea30798c57
Nkrumah <nkrumahthis@gmail.com>
2023-02-10 08:04:45 +0000

    fix sql

--- main.sql
+++ main.sql
@@ -1,3 +1,4 @@
+-- Active: 1676015698801@@127.0.0.1@3306
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -10,22 +11,23 @@ CREATE TABLE IF NOT EXISTS customers (
     name VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
     token VARCHAR(255) NOT NULL
-)
+);
+
 CREATE TABLE IF NOT EXISTS orders (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     packs INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
+    customer_id INTEGER NOT NULL,
     location VARCHAR(255) NOT NULL,
     status VARCHAR(255) NOT NULL CHECK (status IN ('PENDING', 'FULFILLED', 'CANCELLED')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
 ); 
 CREATE TABLE IF NOT EXISTS payments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     amount MONEY NOT NULL,
-    user_id VARCHAR(255) NOT NULL,
+    customer_id VARCHAR(255) NOT NULL,
     order_id VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
     FOREIGN KEY (order_id) REFERENCES orders(id)
 ); 
--- seed.sql
+++ seed.sql
@@ -1,9 +1,13 @@
-INSERT OR REPLACE INTO users(id, name, phone) VALUES
-    (1, 'Adam Asante', '+1234567890', "password"),
-    (2, 'Bill Boakye', '+9872340192', "password"),
-    (3, 'Charles C', '+7232019923', "password");
+INSERT OR REPLACE INTO users(id, name, email, password) VALUES
+    (1, 'Adam Asante', 'aa@assortedjollof.com', "password"),
+    (2, 'Bill Boakye', 'bb@assortedjollof.com', "password"),
+    (3, 'Charles C', 'cc@assortedjollof.com', "password");
 
-INSERT OR REPLACE INTO orders(id, packs, user_id, location, status, created_at) VALUES
+INSERT OR REPLACE INTO customers(id, name, phone, token) VALUES
+    (1, 'Derek Darko', '+1234567890', "1234"),
+    (2, 'Eric Enchil', '+9872340192', "5678"),
+    (3, 'Fred Faraday', '+7232019923', "9012");
+INSERT OR REPLACE INTO orders(id, packs, customer_id, location, status, created_at) VALUES
     (1, 1, 1, 'Ashaiman Junction', 'FULFILLED', NULL),
     (2, 2, 2, 'Burma Camp', 'FULFILLED', NULL),
     (3, 1, 3, 'Next to China Mall', 'CANCELLED', NULL),
@@ -14,7 +18,7 @@ INSERT OR REPLACE INTO orders(id, packs, user_id, location, status, created_at) 
     (8, 2, 1, 'Ashaiman Junction', 'PENDING', NULL),
     (9, 2, 2, 'Ashaiman Junction', 'PENDING', NULL);
 
-INSERT OR REPLACE INTO payments(id, amount, user_id, order_id, created_at) VALUES
+INSERT OR REPLACE INTO payments(id, amount, customer_id, order_id, created_at) VALUES
     (1, 45.00, 1, 1, NULL),
     (2, 90.00, 2, 2, NULL),
     (3, 135.00, 2, 5, NULL),
