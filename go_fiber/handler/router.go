4039d87ae3bb44859433701a90f59911ad683f9c
Nkrumah <nkrumahthis@gmail.com>
2023-02-12 19:44:02 +0000

    move router

---
+++ go_fiber/handler/router.go
@@ -0,0 +1,12 @@
+package handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+	app.Get("/users", GetUsers)
+	app.Get("/users/:id", GetUser)
+
+	app.Get("/orders", GetOrders)
+}
