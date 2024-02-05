471de9216a3e8f1c2278467b8584a4f4712882d8
Nkrumah <nkrumahthis@gmail.com>
2024-02-05 22:29:43 +0000

    start java spring web project

---
+++ java_spring/src/main/java/com/nkrumahsarpong/assortedjollof/AssortedJollofApplication.java
@@ -0,0 +1,13 @@
+package com.nkrumahsarpong.assortedjollof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssortedJollofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ java_spring/src/main/java/com/nkrumahsarpong/assortedjollof/Greeting.java
@@ -0,0 +1,5 @@
+package com.nkrumahsarpong.assortedjollof;
+
+public record Greeting(long id, String content) {
+    
+}
