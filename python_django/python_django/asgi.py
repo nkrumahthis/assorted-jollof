085c85872385a391a475c661d9343b6342631110
Nkrumah <nkrumahthis@gmail.com>
2024-02-19 00:16:28 +0000

    Revert "rename python_django app to assorted_jollof"
    This reverts commit 25f31c4e1cdc37c8af0e56f7a57349465dd2f5ca.

--- python_django/python_django/asgi.py
+++ python_django/python_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for assorted_jollof project.
+ASGI config for python_django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assorted_jollof.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_django.settings')
 
 application = get_asgi_application()
