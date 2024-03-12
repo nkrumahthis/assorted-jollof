d8024e25f27ec5de9086247cd00173383ac55629
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-03-12 23:48:01 +0000

    create payment

--- php_lumen/app/Http/Controllers/PaymentController.php
+++ php_lumen/app/Http/Controllers/PaymentController.php
@@ -18,12 +18,16 @@ class PaymentController extends Controller
 
     public function create($request)
     {
-        $this->validate($request, [
-
+        $validated = $request->validate([
+            "amount" => 'required|float',
+            'customer_id' => 'required|integer',
+            'order_id' => 'required|integer'
         ]);
 
         return Payment::create([
-
+            'amount' => $request->amount,
+            'customer_id' => $request->customer_id,
+            'order_id' => $request->order_id
         ]);
     }
 
